792effffb118736194f992e45655cd4ed9c7cd5d
debrasali <deandrobrasali@gmail.com>
2025-05-01 22:50:34 +0800

    Create animation.h

---
+++ animation.h
@@ -0,0 +1,11 @@
+#ifndef ANIMATION_H
+#define ANIMATION_H
+
+#include <string>
+
+void initConsole();
+void typePrint(const std::string& text, int delayMs = 5);
+void clearScreenAnimate();
+void displayAnimatedMenu();
+
+#endif // ANIMATION_H 
