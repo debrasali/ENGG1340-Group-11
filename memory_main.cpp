11aa99fb012e46810bbc0af4a95d249bca852764
Bradley <bradleywilz72@gmail.com>
2025-05-03 15:47:10 +0800

    Update memory_main.cpp

--- memory_main.cpp
+++ memory_main.cpp
@@ -352,13 +352,13 @@ int main() {
 
                 // Difficulty selection roasts 
                 if (diffChoice == 1) {
-                    std::cout << "Aww, scared of a challenge? Don't worry, we'll go easy on you....\n";
+                    std::cout << "\nAww, scared of a challenge? Don't worry, we'll go easy on you....\n";
                 }    else if (diffChoice == 2) {
-                        std::cout << "Ah, the 'I'm kinda serious but still value my sanity' pick. Bold.\n";
+                        std::cout << "\nAh, the 'I'm kinda serious but still value my sanity' pick. Bold.\n";
                 }    else if (diffChoice == 3) {
-                        std::cout << "Someone's feeling brave! Or just stupid. Either way, good luck - you'll need it.\n";
+                        std::cout << "\nSomeone's feeling brave! Or just stupid. Either way, good luck - you'll need it.\n";
                 }    else if (diffChoice == 4) {
-                        std::cout << "Oh. Oh no. You're one of *those* people... Absolute madlad. Enjoy the suffering.\n";
+                        std::cout << "\nOh. Oh no. You're one of *those* people... Absolute madlad. Enjoy the suffering.\n";
                 }
                 
                 std::this_thread::sleep_for(std::chrono::milliseconds(1500)); // 1.5 second pause
